de944ce3240137360308056c918b64099923bde2
Eli Cusic <tntitans31@gmail.com>
2020-10-03 14:25:08 -0500

    filled out UsersController - still need error pages built out, though

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
 
+    get '/users/signup' do
+        erb :"users/signup"
+    end
+
+    post '/users' do
+        @user = User.new(params)
+        # revisit to see if refactoring would be good
+        if @user && @user.save
+            session[:user_id] = @user.id
+            redirect '/users/homepage'
+        else
+        # need to build out error page for redirection
+            redirect '/error'
+        end
+    end
+
 end
