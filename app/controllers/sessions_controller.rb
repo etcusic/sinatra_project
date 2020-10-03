6c5b566d8cba76d6f36a8e33111053d7fe41ff6d
Eli Cusic <tntitans31@gmail.com>
2020-10-03 14:14:12 -0500

    filled out SessionsController - still need to figure out error redirection, though

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,24 @@
+require_relative 'sessions_controller.rb'
+
 class SessionsController < ApplicationController
 
+    get '/sessions/login' do
+        erb :'sessions/login'
+    end
+
+    post '/login' do
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/users/homepage'
+        else 
+            # Need to develop errors for redirection
+            redirect '/errors'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/landing_page'
+    end
 end
