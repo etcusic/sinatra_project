a8b1ff09dd7d8fc9260a69b1cfde65884ee3d2e8
Eli Cusic <tntitans31@gmail.com>
2020-10-05 08:58:31 -0500

    set up skeleton of TeamsController

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,3 +1,31 @@
 class TeamsController < ApplicationController
 
+    get '/teams'do
+        erb :"teams/index"
+    end
+
+    get '/teams/new' do
+        erb :"teams/new"
+    end
+
+    get '/teams/:id' do
+        erb :"teams/show"
+    end
+
+    get '/teams/:id/edit' do
+        erb :"teams/edit"
+    end
+
+    post '/teams' do
+        # redirect "/teams/#{@team.id}"
+    end
+
+    patch '/teams/:id/edit' do
+        # redirect "/teams/#{@team.id}"
+    end
+
+    delete '/teams/:id/delete' do
+        # redirect "/teams/#{@team.id}"
+    end
+
 end
