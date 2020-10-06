108a5170f66c1eeeaaca8370fc0af49698801cfb
Eli Cusic <tntitans31@gmail.com>
2020-10-06 11:15:51 -0500

    Refactoring UsersController and TeamsController to account for unauthorized users. Also, made changes to views to give different pages for unauthorized users - and NACHOS

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,9 @@
 class Team < ActiveRecord::Base
     
-    belongs_to :team
+    belongs_to :user
     has_many :players
+
+    validates :name, :location, presence: true
     
     def self.under_capacity?
         self.all.count <= 12
@@ -26,4 +28,23 @@ class Team < ActiveRecord::Base
         roster = Player.all.select{|player| player.team_id == self.id}
     end
 
+    def owner
+        User.all.select{| user | user.id == self.user_id } [0]
+    end
 end
+
+# teams = Team.all
+# teams.each do |team|
+#     if team.user_id
+#         qb = Player.all.detect{|player| !player.team_id && player.position == "QB"} [0]
+#         kicker = Player.all.detect{|player| !player.team_id && player.position == "K"} [0]
+#         rb = Player.all.select{|player| !player.team_id && player.position == "RB"} 
+#         wr = Player.all.select{|player| !player.team_id && player.position == "WR"} 
+#         qb.update(team_id = self.id)
+#         rb[0].update(team_id = self.id)
+#         rb[1].update(team_id = self.id)
+#         wr[0].update(team_id = self.id)
+#         wr[1].update(team_id = self.id)
+#         kicker.update(team_id = self.id)
+#     end
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,23 @@
 class User < ActiveRecord::Base
+    
     has_secure_password
+
+    validates :username, :password, presence: true
+    validates :username, uniqueness: true
     
     has_many :teams 
-    has_many :players, through: :team 
+    # has_many :players, through: :team
     
+    def maximum_number_of_teams?
+        user_teams = Team.all.select{|team| team.user_id == self.id}
+        user_teams.length >= 4
+    end
+
+    # def all_teams
+    #     Team.all.select{|team| team.user_id == self.id}
+    # end
+
+    # def all_players
+        
+    # end
 end
